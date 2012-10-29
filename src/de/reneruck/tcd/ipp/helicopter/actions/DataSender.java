89da4b046bee7efa1b3592648a7d922a05276dae
Rene <reneruck@googlemail.com>
2012-10-29 17:12:07 +0000

    raised sleeping time

--- src/de/reneruck/tcd/ipp/helicopter/actions/DataSender.java
+++ src/de/reneruck/tcd/ipp/helicopter/actions/DataSender.java
@@ -37,7 +37,7 @@ public class DataSender extends Thread {
 				}
 			}
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(2000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
